2570e3e1b860089dd54afc7c79bdd6909a47ac50
Saskuuch <ethansamuelwarner@gmail.com>
2024-11-08 14:01:52 -0800

    (Hopefully) Fixed map

--- main.py
+++ main.py
@@ -33,10 +33,9 @@ def map_create():
 
     map = folium.Map(location=[lat, lon],
                      zoom_start=zoom, control_scale=True)
-    dashboardHTML = render_template("dashboard.html")
     folium.Marker(
         [50.667, -120.367],
-        popup=folium.Popup(dashboardHTML, max_width=900),
+        popup=folium.Popup(getDashboardHTML(), max_width=900),
         tooltip="TRU",
     ).add_to(map)
 
@@ -167,5 +166,168 @@ def addGasses():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=2300)
+
+
+def getDashboardHTML():
+    html = """
+<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="{{ url_for('static', filename='javascript/gaugeFunctions.js') }}" type='text/javascript'></script>  
+<style>
+  .card-body{
+    padding:0;
+  }
+  .col-3{
+    padding:0;
+  }
+  table{
+    display:inline-block;
+  }
+</style>
+<div style='width:900px;height:500px; background-color:white'>
+    <div class="container mt-5" style = "max-width: fit-content;">
+        <!-- Row 1: Sensor title -->
+        <div class="row title-row">
+          <div class="col-12">
+              <h1 class="heading text-center">Sensor 1</h1>
+          </div>
+      </div>
+        
+        <!-- Rows 2-5: 4 equally sized boxes -->
+        <div class="row">
+            <div class="col-3">
+                <div class="card" style="border:0; text-align: center;">
+                    <div class="card-body" id = "methaneGauge">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="border:0; text-align: center;">
+                    <div class="card-body" id = "carbonGauge">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="border:0; text-align: center;">
+                    <div class="card-body" id = "aqGauge">
+                       
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card" style="border:0; text-align: center;">
+                    <div class="card-body" id = "butaneGauge">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body" >
+                        <canvas id = "methaneGraph_1h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body" >
+                        <canvas id = "carbonGraph_1h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body" >
+                        <canvas id = "aqGraph_1h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body" >
+                        <canvas id = "butaneGraph_1h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <canvas id = "methaneGraph_24h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <canvas id = "carbonGraph_24h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <canvas id = "aqGraph_24h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <canvas id = "butaneGraph_24h" width="800" height="800"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        Sensor 13
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        Sensor 14
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        Sensor 15
+                    </div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        Sensor 16
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="{{ url_for('static', filename='javascript/graphFunctions.js') }}" type='text/javascript'></script>
+<script>
+
+</script>
+</html>
+"""
+    return html
 
 
