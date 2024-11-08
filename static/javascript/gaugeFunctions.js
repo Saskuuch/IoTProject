ad9cc5e6ef7bf4278ed62ae5c3f2e2fbda87cfd9
jaxezo <jaxezo@proton.me>
2024-11-08 15:03:49 -0800

    Webpage works

--- static/javascript/gaugeFunctions.js
+++ static/javascript/gaugeFunctions.js
@@ -1,84 +1,106 @@
-google.charts.load('current', {'packages':['gauge']});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-
-var methdata = google.visualization.arrayToDataTable([
-['Label', 'Value'],
-['Methane', 102]
-]);
+// Define options globally
 var methoptions = {
-min:0, max:10000,
-width: 400, height: 140,
-redFrom: 8000, redTo: 10000,
-yellowFrom:1000, yellowTo: 8000,
-minorTicks: 20
+    min: 0, max: 10000,
+    width: 400, height: 140,
+    redFrom: 8000, redTo: 10000,
+    yellowFrom: 1000, yellowTo: 8000,
+    minorTicks: 20
 };
-var carbondata = google.visualization.arrayToDataTable([
-['Label', 'Value'],
-['CO', 5]
 
-]);
 var carboptions = {
-min:0, max:1000,
-width: 400, height: 140,
-redFrom: 500, redTo: 1000,
-yellowFrom:50, yellowTo: 500,
-minorTicks: 20
+    min: 0, max: 1000,
+    width: 400, height: 140,
+    redFrom: 500, redTo: 1000,
+    yellowFrom: 50, yellowTo: 500,
+    minorTicks: 20
 };
-var aqdata = google.visualization.arrayToDataTable([
-['Label', 'Value'],
-['Air Quality', 22]
-]);
+
 var aqoptions = {
-min:0, max:1000,
-width: 400, height: 140,
-redFrom: 700, redTo: 1000,
-yellowFrom:200, yellowTo: 700,
-minorTicks: 20
+    min: 0, max: 1000,
+    width: 400, height: 140,
+    redFrom: 700, redTo: 1000,
+    yellowFrom: 200, yellowTo: 700,
+    minorTicks: 20
 };
-var butdata = google.visualization.arrayToDataTable([
-['Label', 'Value'],
-['Butane', 33]
-]);
 
 var butoptions = {
-min:0, max:1600,
-width: 400, height: 140,
-redFrom: 1000, redTo: 1600,
-yellowFrom:700, yellowTo: 1000,
-minorTicks: 30
+    min: 0, max: 1600,
+    width: 400, height: 140,
+    redFrom: 1000, redTo: 1600,
+    yellowFrom: 700, yellowTo: 1000,
+    minorTicks: 30
 };
 
-var methguage = new google.visualization.Gauge(document.getElementById('methaneGauge'));
-var carbguage = new google.visualization.Gauge(document.getElementById('carbonGauge'));
-var aqguage = new google.visualization.Gauge(document.getElementById('aqGauge'));
-var butguage = new google.visualization.Gauge(document.getElementById('butaneGauge'));
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(drawChart);
 
-methguage.draw(methdata, methoptions);
-carbguage.draw(carbondata, carboptions);
-aqguage.draw(aqdata, aqoptions);
-butguage.draw(butdata, butoptions);
+function drawChart() {
+    var methdata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Methane', 102]
+    ]);
+    var carbondata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['CO', 5]
+    ]);
+    var aqdata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Air Quality', 22]
+    ]);
+    var butdata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Butane', 33]
+    ]);
+
+    var methguage = new google.visualization.Gauge(document.getElementById('methaneGauge'));
+    var carbguage = new google.visualization.Gauge(document.getElementById('carbonGauge'));
+    var aqguage = new google.visualization.Gauge(document.getElementById('aqGauge'));
+    var butguage = new google.visualization.Gauge(document.getElementById('butaneGauge'));
+
+    methguage.draw(methdata, methoptions);
+    carbguage.draw(carbondata, carboptions);
+    aqguage.draw(aqdata, aqoptions);
+    butguage.draw(butdata, butoptions);
 }
 
-function sendGuageUpdate(){
+function sendGuageUpdate() {
     $.post("/getGaugeData", {}, function(data){
         updateGuages(data);
     });
 }
-function updateGuages(data){
-    var methguage =document.getElementById('methaneGauge');
-    var carbguage = document.getElementById('carbonGauge');
-    var aqguage = document.getElementById('aqGauge');
-    var butguage = document.getElementById('butaneGauge');
 
-    methguage.setValue(data[0]);
-    carbguage.setValue(data[1]); 
-    aqguage.setValue(data[2]);   
-    butguage.setValue(data[3]);
-    methguage.draw();
-    carbguage.draw();
-    aqguage.draw();
-    butguage.draw();
-    }
+function updateGuages(data) {
+    // Create new data tables for each gauge
+    var methdata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Methane', data[0] || 0]  // Set to 0 if data is null or undefined
+    ]);
+    
+    var carbondata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['CO', data[1] || 0]
+    ]);
 
-    setInterval(sendGuageUpdate, 5000);
+    var aqdata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Air Quality', data[2] || 0]
+    ]);
+
+    var butdata = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Butane', data[3] || 0]
+    ]);
+
+    // Redraw the gauges with updated data
+    var methguage = new google.visualization.Gauge(document.getElementById('methaneGauge'));
+    var carbguage = new google.visualization.Gauge(document.getElementById('carbonGauge'));
+    var aqguage = new google.visualization.Gauge(document.getElementById('aqGauge'));
+    var butguage = new google.visualization.Gauge(document.getElementById('butaneGauge'));
+
+    methguage.draw(methdata, methoptions);
+    carbguage.draw(carbondata, carboptions);
+    aqguage.draw(aqdata, aqoptions);
+    butguage.draw(butdata, butoptions);
+}
+
+setInterval(sendGuageUpdate, 5000);
